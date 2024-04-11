a7300bb383e822f821d3673ba07c4d342182c78d
Matheus Alves Pereira <matt.alpe.dev@gmail.com>
2024-04-11 19:36:59 -0300

    init

---
+++ components/AppNumber.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+const num = useState<number>("random", () => Math.random());
+const refreshNum = () => {
+  num.value = Math.random();
+};
+</script>
+
+<template>
+  <div>
+    <p>
+      Number: <span>{{ num }}</span>
+    </p>
+    <button @click="refreshNum">Refresh</button>
+  </div>
+</template>
