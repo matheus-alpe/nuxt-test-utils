051997b6b61eb6b9f22eafcedc7b3993e70f62e8
Matheus Alves Pereira <matt.alpe.dev@gmail.com>
2024-04-11 19:37:50 -0300

    test: AppNumber

---
+++ components/AppNumber.test.ts
@@ -0,0 +1,20 @@
+import { it, expect, describe } from "vitest";
+import { mockNuxtImport, mountSuspended } from "@nuxt/test-utils/runtime";
+
+import AppNumber from "./AppNumber.vue";
+
+mockNuxtImport("useState", () => {
+  return () => 3;
+});
+
+describe("AppNumber", () => {
+  it("can mount the component", async () => {
+    const component = await mountSuspended(AppNumber);
+    expect(component.html()).toContain("Number");
+  });
+
+  it("returns the state", async () => {
+    const component = await mountSuspended(AppNumber);
+    expect(component.text()).toContain("Number: 3");
+  });
+});
